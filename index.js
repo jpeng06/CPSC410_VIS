381adebd311146548ff1ecd58b8bc378066807e9
Jordan Brooks <jordbrooks14@gmail.com>
2018-11-26 20:58:13 -0800

    Recursive function detects method calls.

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ const fs = require('fs');
 const directory = process.argv[2];
 const javpar = require('java-parser');
 
-console.log("Analyzing directory: " + directory);
+let methods = {};
+
+console.log('Analyzing directory: ' + directory);
+
+function findMethodInvocation(currMethod, element) {
+    if (typeof element !== 'object') return;
+
+    if (element.node === 'MethodInvocation') {
+        //console.log("GOT ONE: " + currMethod + " calls " + element.name.identifier);
+        let name = element.name.identifier;
+        if (methods[currMethod].calls.hasOwnProperty(name)) {
+            let currentCallNum = methods[currMethod].calls[name];
+            methods[currMethod].calls[name] = currentCallNum + 1;
+        } else {
+            methods[currMethod].calls[name] = 1;
+        }
+    }
+
+    Object.entries(element).forEach(([key, val]) => {
+        if (val) {
+            findMethodInvocation(currMethod, val);
+        }
+    });
+}
 
 fs.readdir(directory, (err, files) => {
 
@@ -19,11 +42,17 @@ fs.readdir(directory, (err, files) => {
            for (let type of x.types) {
                for (let bodyDec of type.bodyDeclarations) {
                    if (bodyDec.node === 'MethodDeclaration') {
-                       console.log(bodyDec.name.identifier);
+                       let methodName = bodyDec.name.identifier;
+                       let methodSize = bodyDec.body.statements.length;
+                       methods[methodName] = { size: methodSize, calls: {} };
+                       for (let statement of bodyDec.body.statements) {
+                           findMethodInvocation(methodName, statement);
+                       }
                    }
                }
            }
+
+           console.log(methods);
         });
     }
-
 });
