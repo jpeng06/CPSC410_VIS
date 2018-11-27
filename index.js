d14a9742fb9658f0f486c3aec979a447acd4a4fc
Jordan Brooks <jordbrooks14@gmail.com>
2018-11-26 18:21:12 -0800

    Parse out all methods.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
 const directory = process.argv[2];
+const javpar = require('java-parser');
 
 console.log("Analyzing directory: " + directory);
 
 fs.readdir(directory, (err, files) => {
-    console.log("Reading...");
-    if (!err) {
-        console.log("No error.");
-        for (let file of files) {
-            console.log(file);
-        }
-    } else {
-        console.log(err);
+
+    if (err) throw err;
+
+    for (let file of files) {
+        fs.readFile(directory + file, (err, contentBuffer) => {
+           if (err) throw err;
+
+           let content = contentBuffer.toString();
+
+           let x = javpar.parse(content);
+
+           for (let type of x.types) {
+               for (let bodyDec of type.bodyDeclarations) {
+                   if (bodyDec.node === 'MethodDeclaration') {
+                       console.log(bodyDec.name.identifier);
+                   }
+               }
+           }
+        });
     }
+
 });
