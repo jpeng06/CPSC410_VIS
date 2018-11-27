bc893ccf7f47984f7147f2a7d03e297d45799fdb
James Leung <v9f9@ugrad.cs.ubc.ca>
2018-11-26 20:46:23 -0800

    updated commenting

--- gitCrawler.js
+++ gitCrawler.js
@@ -1,13 +1,20 @@
 // MARK: - gitCrawler.js
+
 // this program takes in a repo URL and iterates through a number of commits.
 // initially, a tmp directory is created to store current state of branch.
 // then it walks from first commit until the last commit, replacing tmp with that commit each iteration.
 
+// USAGE: run "node gitCrawler.js" in working directory. ensure that tmp folder has been removed prior.
+// Note: current state fails occasionally. likely an ASYNC issue. if it fails, just delete tmp and try again for now.
+
+
 // set up variables. currently grabbing repo from a hardcoded URL; could change this to a command line argument/other.
 const Git = require("nodegit");
-const url = "https://github.com/jpeng06/CPSC410_VIS.git";
-var localPath = require("path").join(__dirname, "tmp");
-var maxCommits = 20;
+
+const url = "https://github.com/jpeng06/CPSC410_VIS.git";     // repo to clone
+var localPath = require("path").join(__dirname, "tmp");       // where we want to put tmp
+var maxCommits = 30;                                          // max # of commits to go back to.
+var currentCommit = null;
 
 // initialize our walker and repo. program fails if tmp has not been deleted.
 console.log("running... if no output, ensure 'tmp' folder is deleted & run again.");
@@ -39,8 +46,9 @@ Git.Clone(url, localPath).then(function(repository) {
     // finally we can iterate through each commit.
     for(var i = (commits.length - 1); i >= 0; i--) {
         const sha = commits[i].sha();
+        currentCommit = commits[i];
 
-        currentRepo.getCommit(commits[i].sha()).then(function(commit) {
+        currentRepo.getCommit(sha).then(function(commit) {
             console.log("checking out commit: " + sha);
             Git.Checkout.tree(currentRepo, commit, {checkoutStrategy: Git.Checkout.STRATEGY.SAFE}).then(function() {
                 // DO THINGS + STUFF...
@@ -48,6 +56,7 @@ Git.Clone(url, localPath).then(function(repository) {
                 // ...
                 console.log("success.");
             });
+            next();
         });
     }
 
